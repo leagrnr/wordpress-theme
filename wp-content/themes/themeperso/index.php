cf7685930d74067d766498c2e8f94fa15e0692ab
ElRacoon <lea.granier@outlook.fr>
2024-03-19 14:27:34 +0100

    init header && footer

--- wp-content/themes/themeperso/index.php
+++ wp-content/themes/themeperso/index.php
@@ -1,25 +1,26 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
-</head>
-<body>
-    
-    <header>
-        <h1><?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
-    </header>
+<?php get_header(); ?>
 
-    <section>
+<section>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article>
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_content(); ?></p>
+                <h2>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+
+                <figure>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail("medium"); ?>
+                    </a>
+                </figure>
+
+                <p><?php the_excerpt(); ?></p>
+
+                <p> Article publié le <?php the_time('j F Y') ?> par <?php the_author(); ?></p>
+                <!-- <p><?php the_content(); ?></p> -->
             </article>
         <?php endwhile; endif; ?>
     </section>
 
-</body>
-</html>
+<?php get_footer(); ?>
